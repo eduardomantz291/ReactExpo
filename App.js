1c665c5d28d4b9294238630c306edc0180ee7ef1
Eduardo Mantz <eduardomantz2112@gmail.com>
2020-08-20 22:32:03 -0300

    ReactApp.
    primeira aplicação.

--- App.js
+++ App.js
@@ -5,17 +5,51 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+      <View style={styles.Mobile}>
+        <Text style={styles.Title}>Titulo Mobile</Text>
+      </View>
+      <View style={styles.Div}>
+        <Text style={styles.TextTitle}>Olá mundo!</Text>
+        <Text style={styles.P}>Minha primeira aplicação mobile!</Text>
+        <StatusBar style="auto" />
+      </View>
+     </View>
+   );
+} 
+ 
+const  styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+     flex: 1,
+     backgroundColor: '#aee2eb',
+     alignItems: 'center',
+     justifyContent: 'center',
+  },
+ 
+  Div: { 
+    backgroundColor: 'white',
+    width: 250, 
+    height: 230,
+    borderRadius: 15,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  Title: {
+    color: 'white',
+    fontSize: 50,
+    marginTop: -150,
+    textDecorationLine: 'underline', 
+  },
+
+  TextTitle: {
+    fontSize: 30,
+    marginTop: -100,
+    padding: 20,
+  },
+
+  P: {
+    fontSize: 16,
+    padding: 7,
+    textAlign: 'center',
+   }
 });
